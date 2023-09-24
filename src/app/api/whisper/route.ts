09a437b6a84754da3a191ba7282923972b6a6c70
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-09-24 16:51:58 +0100

    start ui

--- src/app/api/whisper/route.ts
+++ src/app/api/whisper/route.ts
@@ -1,20 +1,21 @@
 import OpenAI from "openai";
 import fs from "fs";
 
-// const configuration = ({ apiKey: process.env.OPENAI_API_KEY });
+const configuration = ({ apiKey: process.env.OPENAI_API_KEY });
+console.log(configuration)
 
 
-const openai = new OpenAI({ apiKey: "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" } );
+// const openai = new OpenAI({ apiKey: "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" } );
 
-async function main() {
-  const transcription = await openai.audio.transcriptions.create({
-    file: fs.createReadStream("src/app/Apollinaire-Guillaume_01_Le-Pont-Mirabeau_1913.mp3"),
-    model: "whisper-1",
-  });
+// async function main() {
+//   const transcription = await openai.audio.transcriptions.create({
+//     file: fs.createReadStream("src/app/Apollinaire-Guillaume_01_Le-Pont-Mirabeau_1913.mp3"),
+//     model: "whisper-1",
+//   });
 
-  console.log(transcription.text);
-}
-main();
+//   console.log(transcription.text);
+// }
+// main();
 
 
 

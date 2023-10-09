ed6b879d9f0f45699c6314264b9b28444223a69f
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-10-09 18:35:25 +0100

    file processing complete, printing text to console

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,32 +1,35 @@
-import { writeFile } from 'fs/promises'
-
+import { writeFile, readFile } from 'fs/promises'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { transcribe } from '../whisper/route'
 
 export async function POST(request: NextRequest) {
-  const data = await request.formData()
-  const file: File | null = data.get('file') as unknown as File
+    const data = await request.formData()
+    const file: any | null = data.get('file') as unknown as File
 
-  if (!file) {
-    return NextResponse.json({ success: false })
-  }
+    if (!file) {
+        return NextResponse.json({ success: false })
+    }
 
-  const bytes = await file.arrayBuffer()
-  const buffer = Buffer.from(bytes)
+    try {
+
+        const tempFilePath = `src/app/temporary/uploaded_audio.wav`; // Change the file extension if necessary
 
 
-  try {
-    // Call your main function with the uploaded file
-    // const transcriptionResult = await transcribe(buffer);
-    console.log(bytes)
-    const transcriptionResult = "hello"
+        const buffer = Buffer.from(await file.arrayBuffer());
 
-    return NextResponse.json({ success: true, transcription: transcriptionResult });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ success: false, error: error.message });
-  }
+        await writeFile(tempFilePath, buffer);
+
+        const transcriptionResult = await transcribe(tempFilePath);
+
+
+        return NextResponse.json({ success: true, transcription: transcriptionResult });
+
+    }
+    catch (error) {
+        console.error(error);
+        return NextResponse.json({ success: false, error: error.message });
+    }
 }
 
 
